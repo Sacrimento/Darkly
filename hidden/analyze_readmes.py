2b9fd06a364d1564d3e5908bbc076fe79d26abc8
Sacrimento <sacrimentocorp@gmail.com>
2020-09-19 20:27:54 +0200

    improve hidden flag script

--- hidden/analyze_readmes.py
+++ hidden/analyze_readmes.py
@@ -2,11 +2,13 @@ import os
 
 if __name__ == '__main__':
     seen = set()
+    unique = set()
     for fi in os.listdir('readmes'):
         with open('readmes/'+fi) as f:
             content = f.read()
             if content not in seen:
                 seen.add(content)
-                print('-----------------------------------------')
-                print(content)
-                print('-----------------------------------------')
+                unique.add(content)
+            elif content in seen and content in unique:
+                unique.remove(content)
+    print(*unique)
