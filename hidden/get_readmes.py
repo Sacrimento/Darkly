080926c7e0236e824f9b7928c9a1245a09b05907
Sacrimento <sacrimentocorp@gmail.com>
2020-09-19 21:35:05 +0200

    no need to dl readme + use urllib3

--- hidden/get_readmes.py
+++ hidden/get_readmes.py
@@ -1,35 +1,36 @@
-import requests
+import urllib3
 import sys
-import wget
 import re
 import os
-import shutil
-import uuid
 
 ip = sys.argv[1] if len(sys.argv) > 1 else '192.168.1.240'
 baseurl = 'http://' + ip + '/.hidden/'
 reg = re.compile('<a href=\"([a-z]+\/)\">')
+http = urllib3.PoolManager()
 
-def explore(url):
-    page = requests.get(url)
-    for line in page.iter_lines():
+def explore(url, seen, unique):
+    print('Analyzing', url, end='\r')
+    page = http.request('GET', url)
+    for line in str(page.data).split('\\n'):
         l = str(line[:40])
         m = re.findall(reg, l)
         if m:
-            explore(url + m[0])
+            explore(url + m[0], seen, unique)
         elif 'README' in l:
-            download(url)
+            content = http.request('GET', url + 'README').data
+            analyze(seen, unique, content)
 
-def download(url):
-    print(url)
-    wget.download(url + 'README', out='readmes/' + str(uuid.uuid4()) + '.txt')
+def analyze(seen, unique, content):
+    if content not in seen:
+        seen.add(content)
+        unique.add(content)
+    elif content in seen and content in unique:
+        unique.remove(content)
 
 if __name__ == '__main__':
-    i = input('Are you sure ? this will take long: ')
-    if i not in ('Y', 'y'):
-        exit()
-    if os.path.isdir('readmes'):
-        shutil.rmtree('readmes')
-    os.mkdir('readmes')
-    explore(baseurl)
+    seen = set()
+    unique = set()
 
+    explore(baseurl, seen, unique)
+    
+    print('\n\n Found unique README content :', unique.pop().decode()[:-1])
